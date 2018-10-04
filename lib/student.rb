0bab4e3361cb807797ab9a730df72e07fabba13e
Chris Franklin <cf73021@gmail.com>
2018-10-04 20:58:49 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -83,6 +83,20 @@ class Student
     end
   end
 
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade = 10
+    LIMIT 1
+    ORDER BY id
+    SQL
+
+    DB[:conn].execute(sql, x).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
   def self.create_table
     sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students (
